2297a8aae6bdd73c4af4efa23488619007054f52
MateoNova <83466744+MateoNova@users.noreply.github.com>
2025-02-05 05:08:06 +0100

    Add namespaces

--- Assets/Scripts/RandomWalkGeneration/RandomWalkGenerator.cs
+++ Assets/Scripts/RandomWalkGeneration/RandomWalkGenerator.cs
@@ -2,63 +2,66 @@
 using System.Linq;
 using UnityEngine;
 
-/// <summary>
-/// Generates a random walk dungeon and renders it on a tilemap.
-/// </summary>
-public class RandomWalkGenerator : BaseGenerator
+namespace RandomWalkGeneration
 {
     /// <summary>
-    /// Settings for the random walk algorithm.
+    /// Generates a random walk dungeon and renders it on a tilemap.
     /// </summary>
-    [SerializeField] private RandomWalkSettings randomWalkSettings;
-
-    /// <summary>
-    /// Runs the dungeon generation algorithm and renders the walkable area.
-    /// </summary>
-    protected override void RunGeneration()
-    { 
-        var floorPositions = GenerateWalkableArea();
-        tilemapRenderer.RenderWalkableTiles(floorPositions);
-    }
-
-    /// <summary>
-    /// Generates the walkable area using the random walk algorithm.
-    /// </summary>
-    /// <returns>A HashSet of Vector2Int representing the positions of the walkable tiles.</returns>
-    private HashSet<Vector2Int> GenerateWalkableArea()
+    public class RandomWalkGenerator : BaseGenerator
     {
-        var currentPos = origin;
-        var tiles = new HashSet<Vector2Int>();
+        /// <summary>
+        /// Settings for the random walk algorithm.
+        /// </summary>
+        [SerializeField] private RandomWalkSettings randomWalkSettings;
 
-        for (var i = 0; i < randomWalkSettings.walkIterations; i++)
+        /// <summary>
+        /// Runs the dungeon generation algorithm and renders the walkable area.
+        /// </summary>
+        protected override void RunGeneration()
+        { 
+            var floorPositions = GenerateWalkableArea();
+            tilemapRenderer.RenderWalkableTiles(floorPositions);
+        }
+
+        /// <summary>
+        /// Generates the walkable area using the random walk algorithm.
+        /// </summary>
+        /// <returns>A HashSet of Vector2Int representing the positions of the walkable tiles.</returns>
+        private HashSet<Vector2Int> GenerateWalkableArea()
         {
-            var path = RandomWalkAlgorithm(currentPos, randomWalkSettings.stepsPerIteration);
-            tiles.UnionWith(path);
-            if (randomWalkSettings.randomizeStartPos && tiles.Count > 0)
+            var currentPos = origin;
+            var tiles = new HashSet<Vector2Int>();
+
+            for (var i = 0; i < randomWalkSettings.walkIterations; i++)
             {
-                currentPos = tiles.ElementAt(Random.Range(0, tiles.Count));
+                var path = RandomWalkAlgorithm(currentPos, randomWalkSettings.stepsPerIteration);
+                tiles.UnionWith(path);
+                if (randomWalkSettings.randomizeStartPos && tiles.Count > 0)
+                {
+                    currentPos = tiles.ElementAt(Random.Range(0, tiles.Count));
+                }
             }
+            return tiles;
         }
-        return tiles;
-    }
     
-    /// <summary>
-    /// Performs a random walk starting from the given origin for a specified number of steps.
-    /// </summary>
-    /// <param name="origin">The starting position of the random walk.</param>
-    /// <param name="steps">The number of steps to take in the random walk.</param>
-    /// <returns>A HashSet of Vector2Int representing the path taken by the random walk.</returns>
-    private static HashSet<Vector2Int> RandomWalkAlgorithm(Vector2Int origin, int steps)
-    {
-        var path = new HashSet<Vector2Int> { origin };
-        var currentPos = origin;
-
-        for (var i = 0; i < steps; i++)
+        /// <summary>
+        /// Performs a random walk starting from the given origin for a specified number of steps.
+        /// </summary>
+        /// <param name="origin">The starting position of the random walk.</param>
+        /// <param name="steps">The number of steps to take in the random walk.</param>
+        /// <returns>A HashSet of Vector2Int representing the path taken by the random walk.</returns>
+        private static HashSet<Vector2Int> RandomWalkAlgorithm(Vector2Int origin, int steps)
         {
-            currentPos += Utils.GetRandomCardinalDirection();
-            path.Add(currentPos);
-        }
+            var path = new HashSet<Vector2Int> { origin };
+            var currentPos = origin;
 
-        return path;
+            for (var i = 0; i < steps; i++)
+            {
+                currentPos += Utils.GetRandomCardinalDirection();
+                path.Add(currentPos);
+            }
+
+            return path;
+        }
     }
 }
--- Assets/Scripts/RandomWalkGeneration/RandomWalkSettings.cs
+++ Assets/Scripts/RandomWalkGeneration/RandomWalkSettings.cs
@@ -1,18 +1,21 @@
-[System.Serializable]
-public class RandomWalkSettings
+namespace RandomWalkGeneration
 {
-    /// <summary>
-    /// The number of iterations to perform the random walk.
-    /// </summary>
-    public int walkIterations = 10;
+    [System.Serializable]
+    public class RandomWalkSettings
+    {
+        /// <summary>
+        /// The number of iterations to perform the random walk.
+        /// </summary>
+        public int walkIterations = 10;
 
-    /// <summary>
-    /// The number of steps to take in each iteration of the random walk.
-    /// </summary>
-    public int stepsPerIteration = 10;
+        /// <summary>
+        /// The number of steps to take in each iteration of the random walk.
+        /// </summary>
+        public int stepsPerIteration = 10;
 
-    /// <summary>
-    /// Whether to randomize the starting position after each iteration.
-    /// </summary>
-    public bool randomizeStartPos = true;
+        /// <summary>
+        /// Whether to randomize the starting position after each iteration.
+        /// </summary>
+        public bool randomizeStartPos = true;
+    }
 }
