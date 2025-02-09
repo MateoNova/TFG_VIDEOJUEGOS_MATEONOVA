7318654671559e1c0f7d406a990d75e8c6e3c245
MateoNova <83466744+MateoNova@users.noreply.github.com>
2025-02-09 16:54:18 +0100

    Add class comment for RandomWalkRoomsSettings

--- Assets/Scripts/RandomWalkGeneration/RandomWalkRoomsSettings.cs
+++ Assets/Scripts/RandomWalkGeneration/RandomWalkRoomsSettings.cs
@@ -1,5 +1,8 @@
 namespace RandomWalkGeneration
 {
+    /// <summary>
+    /// Settings for the random walk rooms generation.
+    /// </summary>
     [System.Serializable]
     public class RandomWalkRoomsSettings
     {
