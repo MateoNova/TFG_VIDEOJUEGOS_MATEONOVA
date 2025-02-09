aee053bfe6fd1abc7c3435cdac68e105d12e3d45
MateoNova <83466744+MateoNova@users.noreply.github.com>
2025-02-09 16:41:23 +0100

    Enhance ConditionalFieldAttribute class with XML documentation for clarity and improved usability

--- Assets/Scripts/ConditionalFieldAttribute.cs
+++ Assets/Scripts/ConditionalFieldAttribute.cs
@@ -1,11 +1,21 @@
 ﻿using System;
 using NUnit.Framework;
 
+/// <summary>
+/// Attribute to conditionally display a field in the Unity Inspector based on the value of another field.
+/// </summary>
 [AttributeUsage(AttributeTargets.Field)]
 public class ConditionalFieldAttribute : PropertyAttribute
 {
+    /// <summary>
+    /// Gets the name of the field that determines the condition.
+    /// </summary>
     public string ConditionFieldName { get; private set; }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ConditionalFieldAttribute"/> class.
+    /// </summary>
+    /// <param name="conditionFieldName">The name of the field that determines the condition.</param>
     public ConditionalFieldAttribute(string conditionFieldName)
     {
         ConditionFieldName = conditionFieldName;
