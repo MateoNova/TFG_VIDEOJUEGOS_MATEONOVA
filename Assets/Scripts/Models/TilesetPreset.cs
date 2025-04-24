f8b215c74d83bd03a4c87da3701b79e86884f358
MateoNova <83466744+MateoNova@users.noreply.github.com>
2025-04-24 21:45:31 +0200

    Refactor wall position determination logic for improved clarity and accuracy

--- Assets/Scripts/Models/TilesetPreset.cs
+++ Assets/Scripts/Models/TilesetPreset.cs
@@ -84,88 +84,116 @@ namespace Models
         [SerializeField] public TileBase doorClosed;
         [SerializeField] public TileBase doorOpen;
 
-        // Autotile mask lookup
         public TileBase[] maskTiles = new TileBase[256];
 
 #if UNITY_EDITOR
         private void OnValidate()
         {
-            // Asegurar tamaño
-            if (maskTiles == null || maskTiles.Length != 256)
-                maskTiles = new TileBase[256];
+            EnsureMaskSize();
 
-            for (int mask = 0; mask < 256; mask++)
+            for (var mask = 0; mask < maskTiles.Length; mask++)
             {
-                // Bits de vecinos
-                bool n = (mask & (1 << 0)) != 0;
-                bool ne = (mask & (1 << 1)) != 0;
-                bool e = (mask & (1 << 2)) != 0;
-                bool se = (mask & (1 << 3)) != 0;
-                bool s = (mask & (1 << 4)) != 0;
-                bool sw = (mask & (1 << 5)) != 0;
-                bool w = (mask & (1 << 6)) != 0;
-                bool nw = (mask & (1 << 7)) != 0;
-
-                Utils.Utils.WallPosition wp;
-
-                //REGLAS DE ASIGNACION DE PAREDES -> FALSE ES PARED TRUE ES WALL
-
-                if (n && ne && e && !w) wp = Utils.Utils.WallPosition.TopRightInner;
-                else if (n && !e && (sw || nw) && w) wp = Utils.Utils.WallPosition.TopLeftInner;
-                else if (!n && e && se && s && !w) wp = Utils.Utils.WallPosition.BottomRightInner;
-                else if (!n && !e && s && (sw || nw) && w) wp = Utils.Utils.WallPosition.BottomLeftInner;
-                else if (!n && e && !s && w) wp = Utils.Utils.WallPosition.Alone;
-                else if (!n && !e && !s && w && (sw || nw) && (ne || se))
-                    wp = Utils.Utils.WallPosition.TripleExceptLeftInner;
-                else if (!n && e && !s && (ne || se) && (nw || sw)) wp = Utils.Utils.WallPosition.TripleExceptRightInner;
-                else if (!n && !e && !w && ne && nw) wp = Utils.Utils.WallPosition.TripleExceptDown;
-                else if (!e && s && !w) wp = Utils.Utils.WallPosition.Up;
-                else if (n && (ne || nw) && !e && !s && (sw || se)) wp = Utils.Utils.WallPosition.TripleExceptUp;
-                else if (!n && !e && !s && !w &&
-                         ((nw && ne) || (nw && se) || (nw && sw && (ne || se)) || (ne && sw) || (ne && se && (nw||sw)) || (sw && se)))
-                    wp = Utils.Utils.WallPosition.AllWallCorner;
-                else if (!n && !e && !s && ne && se) wp = Utils.Utils.WallPosition.TripleExceptLeft;
-                else if (!n && !w && !s && nw && sw) wp = Utils.Utils.WallPosition.TripleExceptRight;
-                else if (n && !e && !w) wp = Utils.Utils.WallPosition.Down;
-                
-                else if (!s && sw && !w) wp = Utils.Utils.WallPosition.TopRight;
-                else if (!n && !s && w) wp = Utils.Utils.WallPosition.Right;
-                
-                else if (!n && !w && nw) wp = Utils.Utils.WallPosition.BottomRight;
-                else if (!n && !e && ne) wp = Utils.Utils.WallPosition.BottomLeft;
-                else if (!n && !s && e) wp = Utils.Utils.WallPosition.Left;
-                else if (!e && se && !s) wp = Utils.Utils.WallPosition.TopLeft;
-                else wp = Utils.Utils.WallPosition.Alone;
-
-                // Asignación final de TileBase
-                maskTiles[mask] = wp switch
-                {
-                    Utils.Utils.WallPosition.Up => upWall,
-                    Utils.Utils.WallPosition.Down => downWall,
-                    Utils.Utils.WallPosition.Left => leftWall,
-                    Utils.Utils.WallPosition.Right => rightWall,
-                    Utils.Utils.WallPosition.TopLeft => topLeftWall,
-                    Utils.Utils.WallPosition.TopRight => topRightWall,
-                    Utils.Utils.WallPosition.BottomLeft => bottomLeftWall,
-                    Utils.Utils.WallPosition.BottomRight => bottomRightWall,
-                    Utils.Utils.WallPosition.TopLeftInner => topLeftInnerWall,
-                    Utils.Utils.WallPosition.TopRightInner => topRightInnerWall,
-                    Utils.Utils.WallPosition.BottomLeftInner => bottomLeftInnerWall,
-                    Utils.Utils.WallPosition.BottomRightInner => bottomRightInnerWall,
-                    Utils.Utils.WallPosition.TripleExceptUp => tripleExceptUpWall,
-                    Utils.Utils.WallPosition.TripleExceptDown => tripleExcetDownWall,
-                    Utils.Utils.WallPosition.TripleExceptLeft => tripleExceptLeftWall,
-                    Utils.Utils.WallPosition.TripleExceptRight => tripleExceptRightWall,
-                    Utils.Utils.WallPosition.TripleExceptLeftInner => tripleExceptLeftInnerWall,
-                    Utils.Utils.WallPosition.TripleExceptRightInner => tripleExceptRightInnerWall,
-                    Utils.Utils.WallPosition.AllWallCorner => allCornersWall,
-                    Utils.Utils.WallPosition.Alone => aloneWall,
-                    _ => aloneWall
-                };
+                var n = GetNeighborBits(mask);
+                var wp = DetermineWallPosition(n);
+                maskTiles[mask] = GetTileForPosition(wp);
             }
 
             EditorUtility.SetDirty(this);
         }
+
+        private void EnsureMaskSize()
+        {
+            if (maskTiles == null || maskTiles.Length != 256)
+                maskTiles = new TileBase[256];
+        }
+
+        // Extract neighbor bits from mask integer
+        private static bool[] GetNeighborBits(int mask)
+        {
+            // order: N, NE, E, SE, S, SW, W, NW
+            var bits = new bool[8];
+            for (var i = 0; i < 8; i++)
+                bits[i] = (mask & (1 << i)) != 0;
+            return bits;
+        }
+
+        private Utils.Utils.WallPosition DetermineWallPosition(bool[] n)
+        {
+            // shorthand for readability
+            bool N = n[0], NE = n[1], E = n[2], SE = n[3];
+            bool S = n[4], SW = n[5], W = n[6], NW = n[7];
+
+            // 1) Inner corners
+            if (N && NE && E && !W) return Utils.Utils.WallPosition.TopRightInner;
+            if (N && !E && (SW || NW) && W) return Utils.Utils.WallPosition.TopLeftInner;
+            if (!N && E && SE && S && !W) return Utils.Utils.WallPosition.BottomRightInner;
+            if (!N && !E && S && (SW || NW) && W) return Utils.Utils.WallPosition.BottomLeftInner;
+
+            // 2) Alone
+            if (!N && E && !S && W) return Utils.Utils.WallPosition.Alone;
+
+            // 3) Triple inner corners
+            if (!N && !E && !S && W && (SW || NW) && (NE || SE))
+                return Utils.Utils.WallPosition.TripleExceptLeftInner;
+            if (!N && E && !S && (NE || SE) && (NW || SW))
+                return Utils.Utils.WallPosition.TripleExceptRightInner;
+
+            // 4) Triple walls
+            if (!N && !E && !W && NE && NW) return Utils.Utils.WallPosition.TripleExceptDown;
+            if (N && (NE || NW) && !E && !S && (SW || SE))
+                return Utils.Utils.WallPosition.TripleExceptUp;
+
+            // 5) Straight walls
+            if (!E && S && !W) return Utils.Utils.WallPosition.Up;
+            if (N && !E && !W) return Utils.Utils.WallPosition.Down;
+            if (!N && !S && W) return Utils.Utils.WallPosition.Right;
+            if (!N && !S && E) return Utils.Utils.WallPosition.Left;
+
+            // 6) All-corners
+            if (!N && !E && !S && !W &&
+                ((NW && NE) || (NW && SE) || (NW && SW && (NE || SE)) ||
+                 (NE && SW) || (NE && SE && (NW || SW)) || (SW && SE)))
+                return Utils.Utils.WallPosition.AllWallCorner;
+
+            // 7) Triple walls (sides)
+            if (!N && !E && !S && NE && SE) return Utils.Utils.WallPosition.TripleExceptLeft;
+            if (!N && !W && !S && NW && SW) return Utils.Utils.WallPosition.TripleExceptRight;
+
+            // 8) External corners
+            if (!S && SW && !W) return Utils.Utils.WallPosition.TopRight;
+            if (!E && SE && !S) return Utils.Utils.WallPosition.TopLeft;
+            if (!N && !W && NW) return Utils.Utils.WallPosition.BottomRight;
+            if (!N && !E && NE) return Utils.Utils.WallPosition.BottomLeft;
+
+            // default
+            return Utils.Utils.WallPosition.Alone;
+        }
+
+        // Map WallPosition to the corresponding TileBase
+        private TileBase GetTileForPosition(Utils.Utils.WallPosition wp) => wp switch
+        {
+            Utils.Utils.WallPosition.Up => upWall,
+            Utils.Utils.WallPosition.Down => downWall,
+            Utils.Utils.WallPosition.Left => leftWall,
+            Utils.Utils.WallPosition.Right => rightWall,
+            Utils.Utils.WallPosition.TopLeft => topLeftWall,
+            Utils.Utils.WallPosition.TopRight => topRightWall,
+            Utils.Utils.WallPosition.BottomLeft => bottomLeftWall,
+            Utils.Utils.WallPosition.BottomRight => bottomRightWall,
+            Utils.Utils.WallPosition.TopLeftInner => topLeftInnerWall,
+            Utils.Utils.WallPosition.TopRightInner => topRightInnerWall,
+            Utils.Utils.WallPosition.BottomLeftInner => bottomLeftInnerWall,
+            Utils.Utils.WallPosition.BottomRightInner => bottomRightInnerWall,
+            Utils.Utils.WallPosition.TripleExceptUp => tripleExceptUpWall,
+            Utils.Utils.WallPosition.TripleExceptDown => tripleExcetDownWall,
+            Utils.Utils.WallPosition.TripleExceptLeft => tripleExceptLeftWall,
+            Utils.Utils.WallPosition.TripleExceptRight => tripleExceptRightWall,
+            Utils.Utils.WallPosition.TripleExceptLeftInner => tripleExceptLeftInnerWall,
+            Utils.Utils.WallPosition.TripleExceptRightInner => tripleExceptRightInnerWall,
+            Utils.Utils.WallPosition.AllWallCorner => allCornersWall,
+            Utils.Utils.WallPosition.Alone => aloneWall,
+            _ => aloneWall
+        };
 #endif
     }
 }
